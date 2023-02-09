e166eefe2f256a88947026270e1e0b3b40912e0e
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-02-09 07:59:48 +0300

    Add javascript for about section and change background

---
+++ js/about.js
@@ -0,0 +1,19 @@
+const mainMenu = document.querySelector('.mobile-menu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.nav-menu-about');
+const menuitems = document.querySelectorAll('.mobile-menu li a');
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
+function close() {
+  mainMenu.style.top = '-100%';
+}
+// close menu when you click on a menu item
+menuitems.forEach((item) => {
+  item.addEventListener('click', () => {
+    close();
+  });
+});
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
